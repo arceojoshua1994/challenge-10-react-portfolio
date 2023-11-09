67b5b645d0ae1ef383a64ad7e5ced49e9d2c636e
arceojoshua94 <arceojoshua94@outlook.com>
2023-11-08 19:08:11 -0500

    add contact form

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,20 +1,36 @@
 export default function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div className="container-fluid py-5 " id="contact">
+      <div className="container">
+        <div className="position-relative d-flex justify-content-center">
+          <h1 className="display-1 text-uppercase text-white" style={{ WebkitTextStroke: '1px #dee2e6' }}>Contact</h1>
+          <h1 className="position-absolute text-uppercase title-color">Contact Me</h1>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="contact-form text-center">
+              <div id="success"></div>
+              <form name="sentMessage" id="contactForm" noValidate="novalidate" className="form-styled">
+                <div className="form-row">
+                  <div className="control-group col-12 col-md-6 mb-3">
+                    <input type="text" className="form-control p-4" id="name" placeholder="Your Name" required />
+                  </div>
+                  <div className="control-group col-12 col-md-6 mb-3">
+                    <input type="email" className="form-control p-4" id="email" placeholder="Your Email" required />
+                  </div>
+                </div>
+                <div className="control-group mb-3">
+                  <input type="text" className="form-control p-4" id="subject" placeholder="Subject" required />
+                </div>
+                <div className="control-group mb-3">
+                  <textarea className="form-control py-3 px-4" rows="5" id="message" placeholder="Message" required></textarea>
+                </div>
+                <button className="btn btn-outline-primary" type="submit" id="sendMessageButton">Send Message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
